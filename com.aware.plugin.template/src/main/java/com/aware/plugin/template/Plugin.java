51217e5daa5d61c29dcf0d794f083f296b58c419
vedantswain <vedant12118@iiitd.ac.in>
2018-02-03 20:52:53 -0500

    set plugin to basics

--- com.aware.plugin.template/src/main/java/com/aware/plugin/template/Plugin.java
+++ com.aware.plugin.template/src/main/java/com/aware/plugin/template/Plugin.java
@@ -5,11 +5,8 @@ import android.content.ContentValues;
 import android.content.Intent;
 import android.os.Bundle;
 
-import com.aware.Accelerometer;
 import com.aware.Aware;
 import com.aware.Aware_Preferences;
-
-import com.aware.Screen;
 import com.aware.utils.Aware_Plugin;
 
 public class Plugin extends Aware_Plugin {
@@ -70,41 +67,6 @@ public class Plugin extends Aware_Plugin {
             //Initialize our plugin's settings
             Aware.setSetting(this, Settings.STATUS_PLUGIN_TEMPLATE, true);
 
-            /**
-             * Example of how to enable accelerometer sensing and how to access the data in real-time for your app.
-             * In this particular case, we are sending a broadcast that the ContextCard listens to and updates the UI in real-time.
-             */
-            Aware.startAccelerometer(this);
-            Accelerometer.setSensorObserver(new Accelerometer.AWARESensorObserver() {
-                @Override
-                public void onAccelerometerChanged(ContentValues contentValues) {
-                    sendBroadcast(new Intent("ACCELEROMETER_DATA").putExtra("data", contentValues));
-                }
-            });
-
-            Aware.startScreen(this);
-            Screen.setSensorObserver(new Screen.AWARESensorObserver() {
-                @Override
-                public void onScreenOn() {
-
-                }
-
-                @Override
-                public void onScreenOff() {
-
-                }
-
-                @Override
-                public void onScreenLocked() {
-
-                }
-
-                @Override
-                public void onScreenUnlocked() {
-
-                }
-            });
-
             //Enable our plugin's sync-adapter to upload the data to the server if part of a study
             if (Aware.getSetting(this, Aware_Preferences.FREQUENCY_WEBSERVICE).length() >= 0 && !Aware.isSyncEnabled(this, Provider.getAuthority(this)) && Aware.isStudy(this) && getApplicationContext().getPackageName().equalsIgnoreCase("com.aware.phone") || getApplicationContext().getResources().getBoolean(R.bool.standalone)) {
                 ContentResolver.setIsSyncable(Aware.getAWAREAccount(this), Provider.getAuthority(this), 1);
@@ -119,37 +81,6 @@ public class Plugin extends Aware_Plugin {
             //Initialise AWARE instance in plugin
             Aware.startAWARE(this);
 
-            try {
-                //Using Likert scale to get users' rating of the day
-                ESMFactory esmFactory = new ESMFactory();
-
-                ESM_Likert evening_question = new ESM_Likert();
-                evening_question.setLikertMax(5)
-                        .setLikertMinLabel("Awful")
-                        .setLikertMaxLabel("Awesome!")
-                        .setLikertStep(1)
-                        .setTitle("Evening!")
-                        .setInstructions("How would you rate today?")
-                        .setExpirationThreshold(0) //no expiration = shows a notification the user can use to answer at any time
-                        .setNotificationTimeout(2 * 60) //the notification is automatically removed and the questionnaire expired after 5 minutes ( 5 * 60 seconds)
-                        .setSubmitButton("OK");
-
-                esmFactory.addESM(evening_question);
-
-                //Schedule this question for the evening, only if not yet defined
-                Scheduler.Schedule evening = Scheduler.getSchedule(this, "evening_question1.1");
-                if (evening == null) {
-                    evening = new Scheduler.Schedule("evening_question1.1"); //schedule with morning_question as ID
-                    evening.addMinute(3);
-                    evening.setActionType(Scheduler.ACTION_TYPE_BROADCAST); //sending a request to the client via broadcast
-                    evening.setActionClass(ESM.ACTION_AWARE_QUEUE_ESM); //with the action of ACTION_AWARE_QUEUE_ESM, i.e., queueing a new ESM
-                    evening.addActionExtra(ESM.EXTRA_ESM, esmFactory.build()); //add the questions from the factory
-
-                    Scheduler.saveSchedule(this, evening); //save the questionnaire and schedule it
-                }
-            } catch (JSONException e) {
-                e.printStackTrace();
-            }
 
         }
 
